34f65e32c962fd1bf492c287ac7ce25c5d59eb8d
jcastren <joonas_castren@yahoo.com>
2016-11-07 11:26:16 +0200

    Refactored Create Tournament page

--- src/main/java/fi/joonas/veikkaus/controller/TournamentController.java
+++ src/main/java/fi/joonas/veikkaus/controller/TournamentController.java
@@ -1,31 +1,22 @@
 package fi.joonas.veikkaus.controller;
 
-import static fi.joonas.veikkaus.constants.VeikkausConstants.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.validation.Valid;
+import static fi.joonas.veikkaus.constants.VeikkausConstants.TOURNAMENT_URL;
+import static fi.joonas.veikkaus.constants.VeikkausConstants.URL_DELETE;
+import static fi.joonas.veikkaus.constants.VeikkausConstants.URL_MODIFY;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import fi.joonas.veikkaus.Greeting;
 import fi.joonas.veikkaus.dao.TournamentDao;
-import fi.joonas.veikkaus.guientity.TournamentGUI;
 import fi.joonas.veikkaus.jpaentity.Tournament;
 import fi.joonas.veikkaus.service.TournamentService;
 
@@ -33,43 +24,6 @@ import fi.joonas.veikkaus.service.TournamentService;
 @RequestMapping(TOURNAMENT_URL)
 public class TournamentController extends WebMvcConfigurerAdapter {
 
-	    @GetMapping("/createPost")
-	    public String tournamentForm(Model model) {
-	        model.addAttribute("tournamentGUI", new TournamentGUI());
-	        return "tournament";
-	    }
-
-	    @PostMapping("/createPost")
-	    public String tournamentSubmit(@ModelAttribute TournamentGUI tournamentGUI) {
-	    	Long tournamentId = null;
-			try {
-				tournamentId = tournamentService.insert(tournamentGUI);
-			} catch (Exception ex) {
-				logger.error("Error creating the tournament: ", ex);
-				return "Error creating the tournament: " + ex.toString();
-			}
-			logger.debug("Tournament succesfully created with id = " + tournamentId);
-	        return "tournamentresult";
-	    }
-	    
-	    /*
-		// Create tournament
-	    @RequestMapping(value="/createPost", method = RequestMethod.POST)
-	    //public String createTournament (@RequestParam String name, @RequestParam String year, Model model) {
-	    public String createTournament(@ModelAttribute(value="tournament") Tournament tournament, Model model) {
-	        Long tournamentId = null;
-			try {
-				tournamentId = tournamentService.insert(tournament);
-			} catch (Exception ex) {
-				logger.error("Error creating the tournament: ", ex);
-				return "Error creating the tournament: " + ex.toString();
-			}
-			logger.debug("Tournament succesfully created with id = " + tournamentId);
-	        
-	        return "redirect:/veikkaus/tournament/tournaments";
-	    }
-	    */
-	    
 	@Autowired
 	private TournamentService tournamentService;
 	
@@ -78,57 +32,34 @@ public class TournamentController extends WebMvcConfigurerAdapter {
 	
 	private static final Logger logger = LoggerFactory.getLogger(TournamentController.class);
 	
-	@RequestMapping("/tournaments")
-	public String getTournaments(
-			//@RequestParam(value = "tournament", required = false, defaultValue = "Gothia Cup") String tournament,
-			Model model) {
-		
-		/*
-		Tournament tourn = tournamentDao.findOne(Long.valueOf(1));		
-		String tournament = tourn == null ? "Tournament not found" : tourn.getName();
-		model.addAttribute("tournament", tournament);
-		*/
-		
-		/*List<Tournament> tournList = new ArrayList<Tournament>();
-		tournamentDao.findAll();
-		*/
-		
-		model.addAttribute("tournaments", tournamentDao.findAll());
-		
-		
-		/*
-		Tournament tourn1 = new Tournament("Italy", 1990);
-		Tournament tourn2 = new Tournament("USA", 1994);
-		
-		List<Tournament> tournamentList = new ArrayList<Tournament>();
-		tournamentList.add(tourn1);
-		tournamentList.add(tourn2);
-		
-		model.addAttribute("tournaments", tournamentList);
-		*/
-		
-		return "tournaments";
-	}
+	@GetMapping("/create")
+    public String tournamentForm(Model model) {
+        model.addAttribute("tournament", new Tournament());
+        return "createTournament";
+    }
 
 	/**
-	 * GET /create --> Create a new tournament and save it in the database.
+	 * POST /create --> Create a new tournament and save it in the database.
 	 */
-	@RequestMapping(URL_CREATE)
-	//@ResponseBody
-	public String create(@RequestParam String name, @RequestParam String year) {
-		Long tournamentId = null;
+    @PostMapping("/create")
+    public String tournamentSubmit(@ModelAttribute Tournament tournament) {
+    	Long tournamentId = null;
 		try {
-			tournamentId = tournamentService.insert(name, year);
+			tournamentId = tournamentService.insert(tournament);
 		} catch (Exception ex) {
 			logger.error("Error creating the tournament: ", ex);
 			return "Error creating the tournament: " + ex.toString();
 		}
 		logger.debug("Tournament succesfully created with id = " + tournamentId);
-		//return "Tournament succesfully created with id = " + tournamentId;
-		
-		return "redirect:/tournament/tournaments";
-	}
+        return "tournamentresult";
+    }
 	
+	@RequestMapping("/tournaments")
+	public String getTournaments(
+			Model model) {
+		model.addAttribute("tournaments", tournamentDao.findAll());
+		return "tournaments";
+	}
 
 	/**
 	 * GET /delete --> Delete the tournament having the passed id.
--- src/main/java/fi/joonas/veikkaus/service/TournamentService.java
+++ src/main/java/fi/joonas/veikkaus/service/TournamentService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fi.joonas.veikkaus.dao.TournamentDao;
-import fi.joonas.veikkaus.guientity.TournamentGUI;
 import fi.joonas.veikkaus.jpaentity.Tournament;
 
 @Service
@@ -21,10 +20,6 @@ public class TournamentService {
 		return tournamentDao.save(tournament).getId();
 	}
 	
-	public Long insert(TournamentGUI tournament) {
-		return tournamentDao.save(convertToDao(tournament)).getId();
-	}
-	
 	public Long modify(String id, String name, String year) {
 		Tournament tournament = tournamentDao.findOne(Long.valueOf(id)); 
 		tournament.setName(name);
@@ -37,13 +32,6 @@ public class TournamentService {
 		tournamentDao.delete(Long.valueOf(id));
 		succeed = true;
 		return succeed;
-	}
-	
-	private Tournament convertToDao(TournamentGUI guiObj) {
-		Tournament daoObj = new Tournament();
-		daoObj.setName(guiObj.getName());
-		daoObj.setYear(Integer.parseInt(guiObj.getYear()));
-		return daoObj;
 	}
 	
 }
