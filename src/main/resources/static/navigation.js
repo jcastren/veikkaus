07014a7501240780058f3f383098f6d3084e3b88
jcastren <joonas_castren@yahoo.com>
2016-11-10 15:19:38 +0200

    Added navigation to Tourn, Team and Team tourn views

--- src/main/resources/static/navigation.js
+++ src/main/resources/static/navigation.js
@@ -1,45 +1,38 @@
-var babelCss = document.createElement("link");
-babelCss.href = "babel.css";
-babelCss.rel= "stylesheet";
-document.head.appendChild(babelCss);
+$("<link href='/veikkaus/babel.css' rel='stylesheet'>").appendTo("head");
 
-var navigation = document.createElement("nav");
-navigation.role = "navigation";
-navigation.id = "header";
-navigation.class = "navbar navbar-default navbar-fixed-top";
-document.body.appendChild(navigation);
+$("<nav role='navigation' id='header' class='navbar navbar-default navbar-fixed-top'>").appendTo("body");
+$("<div id='container' class='container'>").appendTo("#header");
+$("<div id='navigation' class='collapse navbar-collapse'>").appendTo("#container");
+$("<ul id='navigationList' class='nav navbar-nav'>").appendTo("#navigation");
+$("<li><a class='showTooltip' href='/veikkaus/tournament/getAll' title='Go see tournaments'>Tournaments</a>").appendTo("#navigationList");
+$("<li><a class='showTooltip' href='/veikkaus/team/getAll' title='Go see teams'>Teams</a>").appendTo("#navigationList");
+$("<li><a class='showTooltip' href='/veikkaus/tournamentTeam/getAll' title='Go see tournament teams'>Tourn. teams</a>").appendTo("#navigationList");
+$("<li><a class='showTooltip' href='/veikkaus/player/getAll' title='Go see players'>Players</a>").appendTo("#navigationList");
+$("<li><a class='showTooltip' href='/veikkaus/tournamentPlayer/getAll' title='Go see tournament players'>Tourn. players</a>").appendTo("#navigationList");
+$("<li><a class='showTooltip' href='/veikkaus/game/getAll' title='Go see games'>Games</a>").appendTo("#navigationList");
+$("<li><a class='showTooltip' href='/veikkaus/scorer/getAll' title='Go see scorers'>Scorers</a>").appendTo("#navigationList");
+$("<li><a class='showTooltip' href='/veikkaus/userRole/getAll' title='Go see user roles'>User roles</a>").appendTo("#navigationList");
+$("<li><a class='showTooltip' href='/veikkaus/user/getAll' title='Go see users'>Users</a>").appendTo("#navigationList");
+$("<li><a class='showTooltip' href='/veikkaus/status/getAll' title='Go see statuses'>Statuses</a>").appendTo("#navigationList");
+$("<li><a class='showTooltip' href='/veikkaus/bet/getAll' title='Go see bets'>Bets</a>").appendTo("#navigationList");
+$("<li><a class='showTooltip' href='/veikkaus/betResult/getAll' title='Go see bet results'>Bet results</a>").appendTo("#navigationList");
 
-var container = $("<div class='container'></div>" );
-container.appendTo("#header");
+// Following code snippet loops all items whose id ends with Link and adds a JQuery tooltip to them
+/*$("[id$=Link]").each(function() {
+		$(this).tooltip();
+});*/
 
-var navigationDiv = $("<div id='navigation' class='collapse navbar-collapse'></div>");
-navigationDiv.appendTo(container);
+// However, it's wiser to use classes for this functionality:
+$(".show-tooltip").each(function() {
+		$(this).tooltip();
+});
 
-var ul = $("<ul class='nav navbar-nav'></ul>");
-ul.appendTo(navigationDiv);
-
-var li1 = $("<li><a id='tournamentsLink' href='tournament/getAll' title='Go see tournaments'>Tournaments</a></li>");
-li1.appendTo(ul);
-
+// Adding to href of every anchor object string '/veikkaus' to beginning to get the links work
+// both in static pages and Thymeleaf templates
 /*
-	<nav role="navigation" id="header" class="navbar navbar-default navbar-fixed-top">
-		<div class="container">		
-			<div id="navigation" class="collapse navbar-collapse">
-				<ul class="nav navbar-nav">
-					<li><a id="tournamentsLink" href="tournament/getAll" title="Go see tournaments">Tournaments</a></li>
-					<li><a id="teamsLink" href="team/getAll" title="Go see teams">Teams</a></li>
-					<li><a id="tournamentTeamsLink" href="tournamentTeam/getAll" title="Go see tournament teams">Tourn. teams</a></li>
-					<li><a id="playersLink" href="player/getAll" title="Go see players">Players</a></li>
-					<li><a id="tournamentPlayersLink" href="tournamentPlayer/getAll" title="Go see tournament players">Tourn. players</a></li>
-					<li><a id="gamesLink" href="scorer/getAll" title="Go see games">Games</a></li>
-					<li><a id="scorersLink" href="scorer/getAll" title="Go see scorers">Scorers</a></li>
-					<li><a id="userRolesLink" href="userRole/getAll" title="Go see user roles">User roles</a></li>
-					<li><a id="usersLink" href="user/getAll" title="Go see users">Users</a></li>
-					<li><a id="statusesLink" href="scorer/getAll" title="Go see statuses">Statuses</a></li>
-					<li><a id="betsLink" href="scorer/getAll" title="Go see bets">Bets</a></li>
-					<li><a id="betResultsLink" href="scorer/getAll" title="Go see bet results">Bet results</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-	*/
+$("a").attr("href", function() {
+	console.log("this.href: " + this.href);
+	return "/veikkaus/" + this.href;
+	this.href.
+});
+*/
